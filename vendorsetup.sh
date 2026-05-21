067a2bacf91fa97918cbc517cd17b9ad69a32103
async202 <96576912+async202@users.noreply.github.com>
2026-05-21 16:22:35 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-add_lunch_combo omni_TECNO_LE7-user
-add_lunch_combo omni_TECNO_LE7-userdebug
-add_lunch_combo omni_TECNO_LE7-eng
+#
+# add_lunch_combo omni_TECNO_LE7-user
+# add_lunch_combo omni_TECNO_LE7-userdebug
+# add_lunch_combo omni_TECNO_LE7-eng
