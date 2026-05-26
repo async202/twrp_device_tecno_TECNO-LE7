a8f39ce1eb4e587f036d5936ee6704dd47c42c36
async202 <96576912+async202@users.noreply.github.com>
2026-05-26 17:02:28 +0700

    Update prepdecrypt.sh

--- recovery/root/system/bin/prepdecrypt.sh
+++ recovery/root/system/bin/prepdecrypt.sh
@@ -3,10 +3,11 @@
 # Create temporary mount point
 mkdir /s
 
-SYS_BLOCK="/dev/block/mapper/system_a"
+SLOT=$(getprop ro.boot.slot_suffix)
+SYS_BLOCK="/dev/block/mapper/system$SLOT"
 
 if [ -b "$SYS_BLOCK" ]; then
-    #  Mount system_a as r/o
+    #  Mount system (ext4 or erofs) as r/o
     mount -t erofs $SYS_BLOCK /s || mount -t ext4 $SYS_BLOCK /s
     # Search patch date in build.prop
     PATCHLEVEL=$(grep "ro.build.version.security_patch=" /s/system/build.prop | cut -d'=' -f2)
