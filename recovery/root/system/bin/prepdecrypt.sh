e101680d3f6d3d2dd9d3d45410411e05b2064935
async202 <96576912+async202@users.noreply.github.com>
2026-05-26 16:47:39 +0700

    Create prepdecrypt.sh

---
+++ recovery/root/system/bin/prepdecrypt.sh
@@ -0,0 +1,27 @@
+#!/system/bin/sh
+
+# Create temporary mount point
+mkdir /s
+
+SYS_BLOCK="/dev/block/mapper/system_a"
+
+if [ -b "$SYS_BLOCK" ]; then
+    #  Mount system_a as r/o
+    mount -t erofs $SYS_BLOCK /s || mount -t ext4 $SYS_BLOCK /s
+    # Search patch date in build.prop
+    PATCHLEVEL=$(grep "ro.build.version.security_patch=" /s/system/build.prop | cut -d'=' -f2)
+    # Additional check for GSI
+    if [ -z "$PATCHLEVEL" ]; then
+        PATCHLEVEL=$(grep "ro.build.version.security_patch=" /s/build.prop | cut -d'=' -f2)
+    fi
+    # Spoof sec. patch date
+    if [ ! -z "$PATCHLEVEL" ]; then
+        setprop ro.build.version.security_patch "$PATCHLEVEL"
+    fi
+    # Unmount and cleanup
+    umount /s
+fi
+
+rmdir /s
+
+setprop tw.decrypt.props.ready true
