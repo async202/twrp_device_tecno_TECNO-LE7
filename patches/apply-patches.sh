3e2a3afe3ffb0c9e4a094cfa14ae086cca4f7cff
async202 <96576912+async202@users.noreply.github.com>
2026-06-06 12:24:00 +0700

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Torturing TWRP source code aka replacing /data/recovery to /data/media/TWRP beacuse this shit does not want to write its own .twrps in /data/recovery, only read
-
-if [ -d "bootable/recovery" ]; then
-    echo "-> Replacing /data/recovery to /data/media/TWRP in sources..."
-    
-    find bootable/recovery -type f \( -name "*.cpp" -o -name "*.h" \) -exec sed -i 's|/data/recovery|/data/media/TWRP|g' {} +
-    
-    echo "-> Replace successful!"
-fi
+# Placeholder
+exit 0
