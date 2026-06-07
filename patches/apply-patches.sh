6a3ebdea694b54d314278e28f570a16d5cde838b
async202 <96576912+async202@users.noreply.github.com>
2026-06-07 15:10:14 +0700

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ./bootable/recovery
-patch -p1 < ../../device/Tecno/Camon18P/patches/0001-revert-save-settings-file-in-persist.patch
+patch -p1 < ../../device/tecno/TECNO_LE7x/patches/0001-revert-save-settings-file-in-persist.patch
 cd ../..
 
 exit 0
