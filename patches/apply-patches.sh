0ad913f4653819ac33ff1f2eb1c71df81c035c26
async202 <96576912+async202@users.noreply.github.com>
2026-06-08 17:46:19 +0700

    Revert "Update apply-patches.sh"
    This reverts commit dd9a84289952468562349060c969b0941b4c2e13.

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+patch -p1 -d bootable/recovery < device/tecno/TECNO_LE7x/patches/0001-revert-save-settings-file-in-persist.patch
+
+sed -i 's|"/persist/.twrps"|"/mnt/vendor/persist/.twrps"|g' bootable/recovery/data.hpp
+sed -i 's|"/persist"|"/mnt/vendor/persist"|g' bootable/recovery/partition.cpp
+sed -i 's|"/persist/time/"|"/mnt/vendor/persist/time/"|g' bootable/recovery/partition.cpp
+sed -i 's|"/persist/"|"/mnt/vendor/persist/"|g' bootable/recovery/twrp-functions.hpp
+sed -i 's|"/persist"|"/mnt/vendor/persist"|g' bootable/recovery/data.cpp
+
+grep -r "/persist" bootable/recovery | grep -v "/mnt/vendor/persist"
+
 exit 0
