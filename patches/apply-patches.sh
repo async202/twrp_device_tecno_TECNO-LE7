fdaae7d93ecfa95901ad6293af8db1e28c1ea2f0
async202 <96576912+async202@users.noreply.github.com>
2026-06-09 16:44:40 +0700

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -5,7 +5,8 @@ patch -p1 -d bootable/recovery < device/tecno/TECNO_LE7x/patches/0001-revert-sav
 find bootable/recovery -name "*.orig" -delete
 
 find bootable/recovery -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" \) -exec sed -i 's|"/persist|"/mnt/vendor/persist|g' {} +
-find bootable/recovery -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" \) -exec sed -i 's|#define TW_STORAGE_PATH.*"/data/recovery/"|#define TW_STORAGE_PATH
+
+find bootable/recovery -name "variables.h" -exec sed -i 's|#define TW_STORAGE_PATH.*"/data/recovery/"|#define TW_STORAGE_PATH             "/mnt/vendor/persist/"|g' {} +
 
 echo "Checking for remaining paths..."
 grep -r "/persist" bootable/recovery | grep -v "/mnt/vendor/persist" | grep -v "bootable/recovery/tests/"
