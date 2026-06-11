3333da669d09877db988b8b1adc27bf2b3f9aed6
async202 <96576912+async202@users.noreply.github.com>
2026-06-11 20:37:05 +0700

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -2,19 +2,6 @@
 
 echo "TWRP config path changer"
 
-find bootable/recovery -name "*.orig" -delete
-
-find bootable/recovery -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" -o -name "*.c" \) \
-    -exec sed -i 's|"/data/recovery|"/persist|g' {} +
-
-find bootable/recovery -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" \) \
-    -exec sed -i 's|"/persist|"/persist|g' {} +
-
-echo "Checking for remaining paths..."
-echo "/data/recovery leftovers:"
-grep -r '"/data/recovery' bootable/recovery || echo "ОК: /data/recovery fully removed."
-
-echo "Checking /persist without mnt/vendor:"
-grep -r "/persist" bootable/recovery | grep -v "/persist" | grep -v "bootable/recovery/tests/" || echo "ОК: All paths are unified."
+patch -p1 -d bootable/recovery < device/tecno/TECNO_LE7x/patches/0001-revert-save-settings-file-in-persist.patch
 
 exit 0
