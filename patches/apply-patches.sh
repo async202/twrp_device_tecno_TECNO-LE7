63af03b20fddb15b2416600a6be807d354e897e6
async202 <96576912+async202@users.noreply.github.com>
2026-06-13 16:57:17 +0700

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -4,4 +4,6 @@ echo "TWRP config path changer"
 
 patch -p1 -d bootable/recovery < device/tecno/TECNO_LE7x/patches/0001-revert-save-settings-file-in-persist.patch
 
+patch -p1 -d bootable/recovery < device/tecno/TECNO_LE7x/patches/0002-change-tw-storage-path-to-persist.patch
+
 exit 0
